f58d8023eb672b91cdc2746028b783909566cb96
201631053 <hubot@githhub@com>
2018-04-30 15:59:41 +0900

    add image

--- TableView Test/ViewController.swift
+++ TableView Test/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var data = ["강아지", "고양이", "토끼"]
+    var subData = ["ONE", "TWO", "THREE"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = "section= \(indexPath.section) row=\(indexPath.row)"
+        
+        let myDog = UIImage(named:"동물\(indexPath.row).jpg")
+        cell.imageView?.image = myDog
+        
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
