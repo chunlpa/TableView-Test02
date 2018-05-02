93dd6f1f200d9237200db5ff89f99d690614e7b8
201631053 <hubot@githhub@com>
2018-05-02 14:50:15 +0900

    add alert

--- TableView Test/ViewController.swift
+++ TableView Test/ViewController.swift
@@ -15,23 +15,46 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var myTableView: UITableView!
     
-    
+    // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cell 메모리 재활용
         let identifier = "RE"
         let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
         cell.textLabel?.text = data[indexPath.row]
-        cell.detailTextLabel?.text = "section= \(indexPath.section) row=\(indexPath.row)"
+        cell.detailTextLabel?.text = "section= \(indexPath.section) row= \(indexPath.row)"
         
-        let myDog = UIImage(named:"동물\(indexPath.row).jpg")
-        cell.imageView?.image = myDog
+        let myImage = UIImage(named:"동물\(indexPath.row).jpg")
+        cell.imageView?.image = myImage
         
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let myAlert = UIAlertController(title: data[indexPath.row], message: subData[indexPath.row], preferredStyle: .alert)
+        
+        let libAction = UIAlertAction(title: "section = \(indexPath.section)   row = \(indexPath.row)", style: .default) {
+            (action: UIAlertAction) -> Void in
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) {
+            (action: UIAlertAction) -> Void in
+        }
+        
+        print("section = \(indexPath.section)   row = \(indexPath.row)")
+        print(data[indexPath.row])
+        print(subData[indexPath.row])
+        
+        myAlert.addAction(libAction)
+        myAlert.addAction(cancelAction)
+        present(myAlert, animated: true, completion: nil)
+        
+        
     }
     
     override func viewDidLoad() {
